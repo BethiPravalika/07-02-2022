c63177587f2e92750c19465526549fb91fd59367
Pravalika <Pravalika@DESKTOP-CJIU4FR>
2022-02-07 11:41:24 +0530

    java program print student information

---
+++ 07-02-2022/src/com/javavariables/Example1.java
@@ -0,0 +1,54 @@
+package com.javavariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		
+//Write a java program to print student information
+
+// variable declaration
+		int studentId;
+		String studentName;
+		int studentAge;
+		float studentMarks;
+		String studentGrade;
+		boolean studentResult;
+		String studentAddress;
+		
+		//Variable intilization
+		studentId=3020;
+		studentName="Pravalika";
+		studentAge=24;
+		studentMarks=66.75f;
+		studentGrade="A";
+		studentResult=true;
+		studentAddress="Pune";
+		
+		System.out.println("studentId: ");
+		System.out.println(studentId);
+		
+		System.out.println("studentName: ");
+		System.out.println(studentName);
+		
+		System.out.println("studentAge: ");
+		System.out.println(studentAge);
+
+
+		System.out.println("studentMarks: ");
+		System.out.println(studentMarks);
+		
+		System.out.println("studentGrade: ");
+		System.out.println(studentGrade);
+
+		System.out.println("studentResult: ");
+		System.out.println(studentResult);
+
+
+		System.out.println("studentAddress: ");
+		System.out.println(studentAddress);
+
+		
+	}
+
+}
